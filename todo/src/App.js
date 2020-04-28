9a8e887cffdf3234e3e27f09d5ffaa8ec2a0f2ff
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-04-28 18:41:13 -0500

    the beginnig of the project

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        Todo App is here !
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todo/src/redusers/todoReducer.js
@@ -0,0 +1,14 @@
+//holds all the todos and single todo
+//initial state
+const initState = {
+    todos: [],
+    todo: ''
+}
+
+//actual reduser 
+export default (state = initState, action) => {
+    switch (action.type){
+        default: 
+            return state;
+    }
+}
